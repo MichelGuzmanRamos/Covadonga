e7fed4b9de8089703ac748014f6ba252a4cc71d2
Ing Michel Guzman <Ing Michel Guzman@DESKTOP-GV5FNBO>
2023-09-02 10:30:44 -0600

    de minuscula a MAYUSCULA

--- covadonga/src/main/java/interfaz/usuario.java
+++ covadonga/src/main/java/interfaz/usuario.java
@@ -101,7 +101,7 @@ public class usuario extends javax.swing.JFrame {
         dtRegistro.setDateFormatString("yyyy-MM-dd");
 
         btnClean.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
-        btnClean.setText("Limpiar");
+        btnClean.setText("LIMPIAR");
         btnClean.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnCleanActionPerformed(evt);
